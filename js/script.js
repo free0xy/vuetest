bee3b4d29bbd1df54a4b7018adb6c5b0adfcc53e
Lucky <>
2017-10-23 16:08:58 +0800

    up 1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,81 @@ var dj = {
     num:929
 }
 
+var jubucom = {
+    template:'<i @click=" msg++ ">{{ msg }}</i>',
+    data:function () {
+        return {
+            msg:0
+        }
+    }
+}
+
+Vue.component('zipro',{
+    template:'<div>' +
+    '<h2>我是子组件</h2>' +
+    '<slot>没内容时显示</slot>' +
+    '</div>'
+})
+
+Vue.component('input-com',{
+    props:['value'],
+    template:'<input type="text" ref="input" :value="value" @input="updateVal($event.target.value)">',
+    methods:{
+        updateVal:function (val) {
+            var formatVal = val.trim().slice(0,val.indexOf('.') === -1?val.length:val.indexOf('.')+3);
+            if(formatVal != val){
+                this.$refs.input.value = formatVal;
+            }
+            this.$emit('input',Number(formatVal))
+        }
+    }
+})
+
 Vue.component('licom',{
     props:['titlex'],
     template:'<li>{{ titlex.title }} 　 X</li>'
+});
+Vue.component('my-com',{
+    template:'<span>你好</span>'
+});
+
+Vue.component('xy-com',{
+    props:['mess'],
+    template:'<i>{{ mess }}</i>'
+});
+
+Vue.component('xy-com1',{
+    props:['mess'],
+    template:'<p>{{ mess }}</p>'
+});
+
+Vue.component('son-com',{
+    template:'<span @click="selfadd">{{ msg }}</span>',
+    data:function () {
+        return {
+            msg:0
+        }
+    },
+    methods:{
+        selfadd:function () {
+            this.msg ++;
+            this.$emit('incre-out');
+        }
+    }
 })
 var app = new Vue({
     el:'#app',
     data:{
+        price:'',
+        total:0,
+        xycominput:'',
+        lazytest:'',
+        aaaa:'真',
+        bbbb:'假',
+        sele:'',
+        radioh:'nan',
+        mcheck:[],
+        checke:'',
         inputText:'',
         nexttodoid:3,
         todoList1:[
@@ -38,7 +106,17 @@ var app = new Vue({
         ahref:'http://www.baidu.com',
         seen:false
     },
+    components:{
+        'my-com':jubucom
+    },
     computed:{
+        chnCheck:function () {
+            if(this.checke){
+                return '真'
+            }else{
+                return '假'
+            }
+        },
         reverseMsg:function () {
             return this.message.split('').reverse().join('');
         }
@@ -51,6 +129,12 @@ var app = new Vue({
         }
     },
     methods:{
+        incre:function () {
+            this.total+=1 ;
+        },
+        sayhi:function (txt) {
+            alert(txt)
+        },
         addNew:function () {
             console.log(this.nexttodoid)
             this.todoList1.push({id:this.nexttodoid++,title:this.inputText})
@@ -71,3 +155,7 @@ var app = new Vue({
         console.log(this.a)
     }
 });
+
+function dothis() {
+    alert(2)
+}
